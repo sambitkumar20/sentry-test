8e76462e1f2ab9394bf522ca4c2a323b37f93ddd
Sambit Kumar Nanda <50833547+SAMBIT20@users.noreply.github.com>
2022-10-15 23:14:27 +0530

    add sentry -prod

--- src/App.js
+++ src/App.js
@@ -2,21 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const breakTheCode = () => {
+    throw new Error("Brokken code")
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={breakTheCode}>Break the code</button>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+    dsn: "https://05ce17c729af4704a82382537a521289@o4503988773912576.ingest.sentry.io/4503988823130112",
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
